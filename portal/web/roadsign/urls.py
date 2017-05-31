4d7ed537f4b41a4d4b2b0831bccce009683f1518
Eben Olson <eben.olson@gmail.com>
2017-05-31 18:04:18 -0400

    updates for playlist editor

--- portal/web/roadsign/urls.py
+++ portal/web/roadsign/urls.py
@@ -6,9 +6,13 @@ urlpatterns = [
     # A view named "home" is referenced in a few places.
     # Make sure to update the references if you change or delete this url line!
     url(r"^$", views.home, name="home"),
-    url(r'textpanel/new/$', views.TextPanelCreate.as_view(), name='textpanel_create'),
+    url(r'textpanel/create/$', views.TextPanelCreate.as_view(), name='textpanel_create'),
     url(r'textpanel/update/(?P<pk>\d+)/$', views.TextPanelUpdate.as_view(), name='textpanel_update'),
     url(r'textpanel/delete/(?P<pk>\d+)/$', views.TextPanelDelete.as_view(), name='textpanel_delete'),
+    url(r'playlist/create/$', views.PlaylistCreate.as_view(), name='playlist_create'),
     url(r'playlist/update/(?P<pk>\d+)/$', views.PlaylistUpdate.as_view(), name='playlist_update'),
-    url(r'editor/$', views.PlaylistEditor.as_view(), name='playlist_editor'),
+    url(r'panel/update/(?P<pk>\d+)/$', views.PanelUpdate.as_view(), name='panel_update'),
+    url(r'editor/(?P<pk>\d+)/$', views.PlaylistEditor.as_view(), name='editor'),
+    url(r'playlist/list/$', views.PlaylistList.as_view(), name='playlist_list'),
+    url(r'playlist/delete/(?P<pk>\d+)/$', views.PlaylistDelete.as_view(), name='playlist_delete'),
 ]
--- portal/web/roadsign/views.py
+++ portal/web/roadsign/views.py
@@ -4,14 +4,26 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.views import View
 from django.views.generic.edit import UpdateView, DeleteView
+from django.views.generic.list import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic.edit import ModelFormMixin
-from .models import TextPanel, Playlist, PlaylistItem
+from .models import TextPanel, Playlist, PlaylistItem, SignPanel
 from .forms import SignPanelFormSet
 
 class Home(LoginRequiredMixin, django.views.generic.TemplateView):
     template_name = "home.html"
 home = Home.as_view()
+
+
+class PlaylistList(LoginRequiredMixin, ListView):
+    template_name = "playlist_list.html"
+    model = Playlist
+
+
+class PlaylistDelete(LoginRequiredMixin, DeleteView):
+    model = Playlist
+    def get_success_url(self):
+        return reverse('playlist_list')
 
 
 class PlaylistEditor(LoginRequiredMixin, django.views.generic.TemplateView):
@@ -20,9 +32,9 @@ class PlaylistEditor(LoginRequiredMixin, django.views.generic.TemplateView):
 
 class TextPanelCreate(LoginRequiredMixin, View):
     def get(self, request, *args, **kwargs):
-         panel = TextPanel()
-         panel.save()
-         return TextPanelUpdate.as_view()(request, *args, pk=panel.pk, **kwargs)
+         item = TextPanel()
+         item.save()
+         return TextPanelUpdate.as_view()(request, *args, pk=item.pk, **kwargs)
 
 
 class TextPanelDelete(LoginRequiredMixin, DeleteView):
@@ -34,6 +46,25 @@ class TextPanelUpdate(LoginRequiredMixin, UpdateView):
     template_name = 'models/textpanel.html'
     model = TextPanel
     fields = '__all__'
+
+    def get_success_url(self):
+        return reverse('textpanel_update', kwargs=self.kwargs)
+
+class PanelUpdate(LoginRequiredMixin, UpdateView):
+    def get(self, request, *args, **kwargs):
+         panel = SignPanel.objects.get(pk=self.kwargs['pk'])
+         if isinstance(panel, TextPanel):
+             return TextPanelUpdate.as_view()(request, **kwargs)
+         else:
+             return reverse('home')
+
+class PlaylistCreate(LoginRequiredMixin, View):
+    def get(self, request, *args, **kwargs):
+         item = Playlist()
+         item.name = "Untitled"
+         item.save()
+         kwargs['pk'] = item.pk
+         return PlaylistUpdate.as_view()(request, **kwargs)
 
 
 class PlaylistUpdate(LoginRequiredMixin, UpdateView):
